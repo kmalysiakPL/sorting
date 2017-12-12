dfef828c064d80788f27f4c895c463816518da32
Krzysztof <kursantjava69@gmail.com>
2017-12-12 20:44:02 +0100

    working code

--- src/com/kmalysiak/Main.java
+++ src/com/kmalysiak/Main.java
@@ -1,10 +1,36 @@
 package com.kmalysiak;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //test test test
-        //test2
+        Integer [] nums  = {18,3,4,3,1,192,238,32};
+        Integer [] results = insort(nums);
+
+        System.out.println("results" + Arrays.toString(results) );
+
+
+
+
+
+    }
+
+    public static Integer[] insort (Integer[] numToSort){
+
+
+        for (int i = 0; i < numToSort.length; i++) {
+
+            for (int j = i; j >0 ; j--) {
+                if(numToSort[j-1]>numToSort[j]){
+                    Integer temp =  numToSort[j];
+                    numToSort[j] = numToSort[j-1];
+                    numToSort[j-1]=temp;
+                }
+            }
+
+        }
+
+        return numToSort;
     }
 }
